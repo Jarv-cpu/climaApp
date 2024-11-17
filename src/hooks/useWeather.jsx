f9b29f38d3bba28fdff7f19767c38c999470021b
Jesus <tvjarv8@gmail.com>
2024-11-17 00:34:57 -0400

    arreglando api

--- src/hooks/useWeather.jsx
+++ src/hooks/useWeather.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export function useWeather() {
     const apis = {
-      city: "http://api.openweathermap.org/geo/1.0/direct?q=",
+      city: "https://api.openweathermap.org/geo/1.0/direct?q=",
       weather: "https://api.openweathermap.org/data/2.5/weather?",
       key: "e0d661142d959a8c9b819f648d874ef0"
     }
